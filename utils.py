85f2a9fec7d537da770eef4ca435b176c44d6292
zhulinyv <zhulinyv2005@outlook.com>
2025-03-24 15:59:47 +0800

    :wrench: 更新配置文件

--- utils.py
+++ utils.py
@@ -1,11 +1,9 @@
 import os
 import shutil
 
-if not os.path.exists(path := "./data"):
-    os.mkdir("./data")
-
-if not os.path.exists(path := "./data/jm"):
-    os.mkdir(path)
+for folder in ["./data", "./data/jm"]:
+    if not os.path.exists(folder):
+        os.mkdir(folder)
 
 if not os.path.exists(path := "./data/jm/option.yml"):
     shutil.copyfile(f"{os.path.dirname(os.path.abspath(__file__))}/option.yml", path)
