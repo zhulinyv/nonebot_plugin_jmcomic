ab6f63323c962fc091868116027df34476425819
zhulinyv <zhulinyv2005@outlook.com>
2025-03-24 14:44:52 +0800

    :memo: 更新文档

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 import os
+from importlib.metadata import version
 
 import jmcomic
 from nonebot.adapters.onebot.v11 import (
@@ -7,9 +8,29 @@ from nonebot.adapters.onebot.v11 import (
     Message,
 )
 from nonebot.params import CommandArg
+from nonebot.plugin import PluginMetadata
 from nonebot.plugin.on import on_command
 
 from .utils import *  # noqa
+
+try:
+    __version__ = version("nonebot_plugin_jmcomic")
+except Exception:
+    __version__ = "0.0.0"
+
+__plugin_meta__ = PluginMetadata(
+    name="nonebot-plugin-jmcomic",
+    description="下载禁漫并发送",
+    usage="jm + id",
+    homepage="https://github.com/zhulinyv/nonebot_plugin_jmcomic",
+    type="application",
+    supported_adapters={"~onebot.v11"},
+    config=None,
+    extra={
+        "author": "zhulinyv",
+        "version": __version__,
+    },
+)
 
 jm = on_command("jm", aliases={"JM"}, priority=30, block=True)
 
